8e5bbf2aa538cf7bebfc7cc56f803a48b678c056
huangdant@ecs.vuw.ac.nz <akistar518@gmail.com>
2018-08-12 22:49:54 +1200

    test format branch

--- GraphicBugWolrd/src/Bug.java
+++ GraphicBugWolrd/src/Bug.java
@@ -1,15 +1,15 @@
 import javafx.scene.image.Image;
 
 public class Bug extends Entity{
-//	private double x;
-//	private double y;
+	//	private double x;
+	//	private double y;
 	private double speed =10;
 	private double energy = 40;
 	public Bug(double x, double y, Image i){
 		// for circle
-//		this.setCenterX(x);
-//		this.setCenterY(y);
-//		this.setRadius(10);
+		//		this.setCenterX(x);
+		//		this.setCenterY(y);
+		//		this.setRadius(10);
 		this.setLayoutX(x);
 		this.setLayoutY(y);
 		this.setImage(i);
@@ -20,22 +20,22 @@ public class Bug extends Entity{
 	//make randomly movement
 	public void tick(World world) {
 		//for circle
-//		double d = Math.random();
-//		if(d<0.25) {
-//			//north
-//			this.setCenterY(this.getCenterY()-speed);
-//		}else if(d<0.5) {
-//			//south
-//			this.setCenterY(this.getCenterY()+speed);
-//		}else if(d<0.75) {
-//			//east
-//			this.setCenterX(this.getCenterX()-speed);
-//		}else if(d<1) {
-//			//west
-//			this.setCenterX(this.getCenterX()+speed);
-//		}
-//		this.setFitWidth(this.getFitWidth()+10);
-//		this.setFitHeight(this.getFitHeight()+10);
+		//		double d = Math.random();
+		//		if(d<0.25) {
+		//			//north
+		//			this.setCenterY(this.getCenterY()-speed);
+		//		}else if(d<0.5) {
+		//			//south
+		//			this.setCenterY(this.getCenterY()+speed);
+		//		}else if(d<0.75) {
+		//			//east
+		//			this.setCenterX(this.getCenterX()-speed);
+		//		}else if(d<1) {
+		//			//west
+		//			this.setCenterX(this.getCenterX()+speed);
+		//		}
+		//		this.setFitWidth(this.getFitWidth()+10);
+		//		this.setFitHeight(this.getFitHeight()+10);
 
 
 
@@ -54,11 +54,11 @@ public class Bug extends Entity{
 			this.setLayoutX(this.getLayoutX()+speed);
 		}
 	}
-	
-	
+
+
 	public void move() {
 		double d = Math.random();
-	
+
 		if(d<0.25) {
 			//north
 			this.setTranslateY(this.getTranslateY()-speed);
@@ -83,8 +83,8 @@ public class Bug extends Entity{
 	public void setEnergy(double energy) {
 		this.energy = energy;
 	}
-	
-	
-	
+
+
+
 
 }
